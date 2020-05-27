a840da6ccffe9b1ec0041864ccb885c38175f990
Renzo Oldani <oldanirenzo@gmail.com>
2020-05-27 05:19:23 -0300

    Creo la vista 'Productos'

---
+++ src/views/Productos.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <div 
+    v-for="instrumento in instrumentosData" 
+    :key="instrumento.id">
+      <instrumento-item :instrumentoParam="instrumento"></instrumento-item>
+    </div>
+  </div>
+</template>
+
+
+
+<script>
+import Tarjeta from "@/components/Tarjeta.vue";
+
+export default {
+  name: "Productos",
+  components: {
+    "instrumento-item": Tarjeta
+  },
+  mounted() {
+    this.getTarjetas();
+  },
+  data() {
+    return {
+      instrumentosData: []
+    };
+  },
+  methods: {
+    async getTarjetas() {
+      const datos = await fetch("/data/instrumentos.json")
+        const resJson = await datos.json();
+        console.log(resJson)
+        this.instrumentosData = resJson.instrumentos;
+
+      // // }
+      //  const res = await fetch("/data/instrumentos.json")
+      // const resJson = await res.json()
+      // console.log(resJson)
+      // this.instrumentosData = resJson.instrumentos
+      
+      // console.log(res);
+      // const resJson = await JSON.stringify(res);
+      // console.log(resJson);
+      // this.instrumentosData = await resJson.instrumentos;
+      // console.log("InstrumentosData: " + this.instrumentosData);
+    }
+  }
+};
+</script>
