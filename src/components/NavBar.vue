d71e02c70a3af91f8fbdb6ec063717b46947aecf
Renzo Oldani <oldanirenzo@gmail.com>
2020-05-27 05:17:03 -0300

    Creo el NavBar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="/">Home</b-navbar-brand>
+      <b-navbar-brand href="/productos">Productos</b-navbar-brand>
+      <b-navbar-brand href="/dondeEstamos">Donde Estamos</b-navbar-brand>
+    </b-navbar>
+  </div>
+</template>
