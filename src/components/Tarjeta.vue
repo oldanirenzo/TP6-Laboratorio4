256c68ce3d73b2c2337a9b345f7cfe83bc80027c
Renzo Oldani <oldanirenzo@gmail.com>
2020-05-27 05:18:22 -0300

    Creo la 'Tarjeta' para usar en cada instrumento

---
+++ src/components/Tarjeta.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div>
+      <b-card no-body class="overflow-hidden" style="max-width: 540px;">
+        <b-row no-gutters>
+          <b-col md="6">
+            <b-card-img
+              :src="'/images/'+instrumentoParam.imagen"
+              :alt="instrumentoParam.descripcion"
+              class="rounded-0"
+            ></b-card-img>
+          </b-col>
+          <b-col md="6">
+            <b-card-body :title="instrumentoParam.instrumento">
+              <b-card-text>
+                $ {{instrumentoParam.precio}}
+                <br />
+              </b-card-text>
+            </b-card-body>
+          </b-col>
+        </b-row>
+      </b-card>
+    </div>
+  </div>
+</template>
+
+
+
+<script>
+export default {
+  props: ["instrumentoParam"]
+};
+</script>
